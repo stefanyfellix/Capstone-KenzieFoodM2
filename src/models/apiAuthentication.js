4f9901afe626fe1b5041724ae574b6000c04ae1b
icarobteles <eng.icaroteles@gmail.com>
2022-03-22 09:49:35 -0300

    feat(ApiAuthentication): :sparkles: adiciona métodos para realizar requisição na rota de autenticação da API
    O método 'signUp' é responsável por realizar uma requisição POST na API, cadastrando um usuário e deve retornar um objeto. O método 'login' é responsável por realizar uma requisição POST na API, logando o usuário e deve retornar um token de acesso.

--- src/models/apiAuthentication.js
+++ src/models/apiAuthentication.js
@@ -1,3 +1,41 @@
 export class ApiAuthentication{
+    static userToken = "";
     
+    static async signUp(data) {
+        const response      = await fetch("https://kenzie-food-api.herokuapp.com/auth/register", {
+            method: "POST", // Indica o tipo de requisição GET, POST, PATCH, DELETE
+            headers: {
+            "Content-Type": "application/json", // Indica o tipo de dado da requisição
+            },
+            body: JSON.stringify(data), // Informando as informações do usuário
+        });
+
+        //Se usuário já existe --> {status: 'Error', message: 'User Already Exists!'}
+        //Se usuário não existe --> {createdAt: "2022-03-22T12:35:31.068Z", email: "equipe1-teste2@gmail.com", id: 32, name: "equipe1-teste2", password: "$2a$08$3Pg1qyuXNs5k8lv0iZrYMuC3kPlZTbVUc.4vkb7LG3EvD.bSxx4OK", updatedAt: "2022-03-22T12:35:31.068Z"}
+
+        const responseData  = await response.json();
+        return responseData;
+    }
+
+    static async login(data) {
+        const response          = await fetch(
+            "https://kenzie-food-api.herokuapp.com/auth/login", {
+            method: "POST", // Indica o tipo de requisição GET, POST, PATCH, DELETE
+            headers: {
+                "Content-Type": "application/json", // Indica o tipo de dado da requisição
+            },
+            body: JSON.stringify(data), // Informando as informações do usuário
+            }
+        );
+
+        //A resposta em formato JSON é o token de acesso se o usuário existe, se não é um objeto assim --> {status: 'Error', message: 'User does not exists'}
+        const responseData      = await response.json();
+
+        if (responseData.status !== "Error") {
+            //Deve com baste no status fornecer ao usuário uma indicação de que o login foi mal sucedido.
+            ApiAuthentication.userToken  = responseData;
+        }
+
+        return responseData;
+    }
 }
