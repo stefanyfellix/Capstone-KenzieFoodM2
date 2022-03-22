7f5264757c3d986b403cf0239572faded0042ce3
Thales Renan <thalesrmn@hotmail.com>
2022-03-22 11:02:49 -0300

    Métodos da ApiProduct

---
+++ public/scripts/script.js
@@ -0,0 +1,26 @@
+import { ApiProduct } from "./../../src/models/apiProduct.js"
+import { ApiAuthentication } from "./../../src/models/apiAuthentication.js"
+
+
+const registerProductForm = document.querySelector('#registerProductForm')
+const token = ApiAuthentication.userToken;
+
+const registerNewProduct = (event) => {
+
+  const inputs = event.target
+  const productValues = {}
+
+  for (let i = 0; i < inputs.length; i++) {
+    const { name, value } = inputs[i]
+
+    if (name) {
+      productValues[name] = value
+    }
+  }
+
+  if (productValues.preco > 0) {
+    ApiProduct.register('/my/products', productValues, token)
+  } window.alert("Preço deve ser maior que 0")
+}
+
+registerProductForm.addEventListener('submit', registerNewProduct)
--- src/models/apiProduct.js
+++ src/models/apiProduct.js
@@ -1,3 +1,36 @@
-export class ApiProduct{
-    
+export class ApiProduct {
+  static URL = 'https://kenzie-food-api.herokuapp.com/'
+
+  static async registerProduct(path, data, token) {
+    fetch(`${this.URL}${path}`, {
+      "method": "POST",
+      "headers": {
+        "Authorization": `Bearer ${token}`
+      },
+      "body": JSON.stringify(data)
+    })
+      .then((response) => response)
+  }
+
+  static async getProducts(path, token) {
+    const response = await fetch(`${this.URL}${path}`, {
+      "method": "GET",
+      "headers": {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    const responseData = await response.json()
+    return responseData
+  }
+
+  static async getProducts(path, data, token) {
+    fetch(`${this.URL}${path}`, {
+      "method": "PATCH",
+      "headers": {
+        "Authorization": `Bearer ${token}`
+      },
+      "body": JSON.stringify(data)
+    })
+      .then((response) => response)
+  }
 }
