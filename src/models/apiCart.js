ed31c007bc02a0a44bd0a42b637471546e85bab2
icarobteles <eng.icaroteles@gmail.com>
2022-03-22 11:48:11 -0300

    feat(ApiCart): :sparkles: adiciona métodos para listar, adiciona e remover um produto do carrinho pela rota privada da API

--- src/models/apiCart.js
+++ src/models/apiCart.js
@@ -1,3 +1,42 @@
+//Para usuários cadastrados (se ele não for, o carrinho será apenas pelo localstorage)
 export class ApiCart{
-    
+    static async list(token){
+        const response          = await fetch(`https://kenzie-food-api.herokuapp.com/cart`, {
+            method: "GET", 
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              }
+        });
+        const responseData      = await response.json();
+        //O Retorno é um array de objetos
+        console.log(responseData)
+        return responseData;
+    }
+
+    static async add(data, token){
+        const response = await fetch("https://kenzie-food-api.herokuapp.com/cart/add", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(data),
+          });
+      
+          const responseData = await response.json();
+          return responseData;
+    }
+
+    static async delete(token, id){
+        const response = await fetch(`https://kenzie-food-api.herokuapp.com/cart/remove/${id}`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`
+            }
+          })
+
+        return response;
+    }
 }
